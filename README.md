54b9716b5da94691080edc56a2ddaa8674782a00
Leafney <babycoolzx@126.com>
2016-10-26 22:56:46 +0800

    add database config

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-Ubuntu-16.04 + Mysql-5.7
+Docker + Ubuntu-16.04 + Mysql-5.7
 
 #### parameter
 
-* `MYSQL_ROOT_PWD` : root password   default "mysql"
-* `MYSQL_USER`  : new user
-* `MYSQL_USER_PWD` : new user password
+* `MYSQL_ROOT_PWD` : root Password   default "mysql"
+* `MYSQL_USER`     : new User
+* `MYSQL_USER_PWD` : new User Password
+* `MYSQL_USER_DB`  : new Database for new User
 
 #### build image
 
@@ -18,8 +19,14 @@ $ docker build -t="leafney/ubuntu-mysql" .
 $ docker run --name mysql -v /var/docker_data/mysql/data/:/var/lib/mysql -d -p 3306:3306 leafney/ubuntu-mysql
 ```
 
-#### run a container
+#### run a container with new User and Password
 
 ```
-docker run --name mysql -v /var/docker_data/mysql/data/:/var/lib/mysql -d -p 3306:3306 -e MYSQL_ROOT_PWD=123 -e MYSQL_USER=dev -e MYSQL_USER_PWD=dev leafney/ubuntu-mysql
+$ docker run --name mysql -v /var/docker_data/mysql/data/:/var/lib/mysql -d -p 3306:3306 -e MYSQL_ROOT_PWD=123 -e MYSQL_USER=dev -e MYSQL_USER_PWD=dev leafney/ubuntu-mysql
+```
+
+#### run a container with new Database for new User and Password
+
+```
+$ docker run --name mysql -v /var/docker_data/mysql/data/:/var/lib/mysql -d -p 3306:3306 -e MYSQL_ROOT_PWD=123 -e MYSQL_USER=dev -e MYSQL_USER_PWD=dev -e MYSQL_USER_DB=userdb leafney/ubuntu-mysql
 ```
--- startup.sh
+++ startup.sh
@@ -4,26 +4,38 @@ set -e
 MYSQL_ROOT_PWD=${MYSQL_ROOT_PWD:-"mysql"}
 MYSQL_USER=${MYSQL_USER:-""}
 MYSQL_USER_PWD=${MYSQL_USER_PWD:-""}
+MYSQL_USER_DB=${MYSQL_USER_DB:-""}
 
-echo "Setting up new power user credentials."
+echo "[i] Setting up new power user credentials."
 service mysql start $ sleep 10
 
-echo "Setting root new password."
+echo "[i] Setting root new password."
 mysql --user=root --password=root -e "UPDATE mysql.user set authentication_string=password('$MYSQL_ROOT_PWD') where user='root'; FLUSH PRIVILEGES;"
 
-echo "Setting root remote password."
+echo "[i] Setting root remote password."
 mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_USER_PWD" ]; then
-	echo "Create new User and Password."
-	mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+if [ -n "$MYSQL_USER_DB" ]; then
+	echo "[i] Creating datebase: $MYSQL_USER_DB"
+	mysql --user=root --password=$MYSQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_USER_DB\` CHARACTER SET utf8 COLLATE utf8_general_ci; FLUSH PRIVILEGES;"
 
+	if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_USER_PWD" ]; then
+		echo "[i] Create new User: $MYSQL_USER with password $MYSQL_USER_PWD for new database $MYSQL_USER_DB."
+		mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON \`$MYSQL_USER_DB\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+	else
+		echo "[i] Don\`t need to create new User."
+	fi
 else
-	echo "Don\`t need to create new User."
+	if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_USER_PWD" ]; then
+		echo "[i] Create new User: $MYSQL_USER with password $MYSQL_USER_PWD for all database."
+		mysql --user=root --password=$MYSQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+	else
+		echo "[i] Don\`t need to create new User."
+	fi
 fi
 
 killall mysqld
 sleep 5
-echo "Setting end,have fun."
+echo "[i] Setting end,have fun."
 
 exec "$@"
